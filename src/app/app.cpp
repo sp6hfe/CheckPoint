8beff892dbc43ff8b43ee3c155f81c9fb830d1c2
Grzegorz SP6HFE <sp6hfe@gmail.com>
2025-10-15 23:34:23 +0200

    feat: Card UID test display

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,5 +1,8 @@
 #include <app/app.hpp>
 
+#include <etl/span.h>
+#include <etl/string.h>
+
 #include <logger/ILogger.hpp>
 #include <nfc/INfc.hpp>
 
@@ -17,9 +20,18 @@ bool App::init() {
 }
 
 void App::process() {
-  const auto cardDetails = _nfc.lookupCard();
+  const auto cardDetails = _nfc.lookupCard(200u);
   if (cardDetails.has_value()) {
-    _logger.logf(LogLevel::Info, "app", "Card detected. UID: %s", cardDetails->getUid());
+    etl::string<(2 * hw::interface::INfc::CardDetails::MAX_UID_LEN) + 1> uid_str{};
+    uid_str.clear();
+    const char* hex = "0123456789ABCDEF";
+    const auto uid = cardDetails->getUid();
+    for (const auto byte : uid) {
+      uid_str.push_back(hex[(byte >> 4) & 0x0F]);
+      uid_str.push_back(hex[byte & 0x0F]);
+    }
+
+    _logger.logf(LogLevel::Info, "app", "Card detected. UID: %s", uid_str.data());
   }
 }
 
